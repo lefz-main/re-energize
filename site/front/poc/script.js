af03ebd428d4035f5ece7165cf6a1b6651c80543
ADIMDIEALAPTOP <edimzilkic@gmail.com>
2025-06-06 11:41:06 +0200

    Total power send to db fix ps allemaal samen met dion gedaan

--- site/front/poc/script.js
+++ site/front/poc/script.js
@@ -124,7 +124,7 @@ fetch('KaartConfig.json')
 
             determineWinner();
             updateScoreDisplay(totalPower);
-            console.log(totalPower);
+            return totalPower
         }
 
         function determineWinner() {
@@ -173,6 +173,7 @@ fetch('KaartConfig.json')
 
         document.getElementById('submitScore').addEventListener('click', function () {
             let name = prompt('Please enter your name:', 'BOEM KAKA');
+            totalPower = calculatePower();
             const xhttp = new XMLHttpRequest();
             xhttp.open('POST', `score.php?NAME=${name}&SCORE=${totalPower}`);
             xhttp.send();
